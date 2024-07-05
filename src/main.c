32dd23d9027e51729a5aaebe7a2d88e7e4cb8ad4
ivanleu14@gmail.com <ivanleu14@gmail.com>
2024-07-05 20:07:40 +0300

    remove insert user test

--- src/main.c
+++ src/main.c
@@ -1,14 +1,11 @@
 #include "server.h"
 #include "global.h"
 #include "database.h"
-#include "users.h"
 
 int main() {
 	
 	sqlite3* db;
 	database_connect(&db);
-
-	insert_user(db, "Bro", "BroLeu@gmail.com", "brocantguess");
 
 	Server* server;
 	server = server_init(8080);
